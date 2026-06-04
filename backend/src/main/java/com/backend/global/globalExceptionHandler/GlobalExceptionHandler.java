8ff115df7dc8963e27d3b74cab78e9ba01ebe2ba
LDG1599 <yes388087@gmail.com>
2026-06-05 08:45:17 +0900

    수정ë²

--- backend/src/main/java/com/backend/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/backend/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -6,15 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
