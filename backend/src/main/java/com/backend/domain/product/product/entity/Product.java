ac8a313d51df92fb0d39899945c2391f7c0a216c
LDG1599 <yes388087@gmail.com>
2026-06-05 08:42:09 +0900

    작업2

--- backend/src/main/java/com/backend/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/backend/domain/product/product/entity/Product.java
@@ -1,6 +1,24 @@
 package com.backend.domain.product.product.entity;
 
+import com.backend.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
-public class Product {
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class Product extends BaseEntity {
+    private String beanName;
+    private int price;
+    private String category;
+
+    public Product(String beanName, int price, String category){
+        this.beanName=beanName;
+        this.price=price;
+        this.category=category;
+    }
+
 }
