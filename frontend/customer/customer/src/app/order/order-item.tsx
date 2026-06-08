b5ba4f152203bddbb73d9f8e7b08da024b718c74
WLBLaz <93821714+WhiteLinkBlaze@users.noreply.github.com>
2026-06-08 10:01:53 +0900

    Feat/shopping cart (#58)
    * feat: 주문 목록에서 보일 아이템들 개수 수정 기능
    
    * feat: 주문 목록에서 보이도록 수정
    
    * feat: 상품카드드 기본 이미지 반영 및 클릭시 장바구니로 가도록
    
    * feat: products 페이지 api 연동

--- frontend/customer/customer/src/app/order/order-item.tsx
+++ frontend/customer/customer/src/app/order/order-item.tsx
@@ -1,30 +1,40 @@
-import Image from "next/image"
-export function OrderItem({name="", category="", price=0, imageUrl="/file.svg"}){
-    const quantity=1;
-    return (
-        <>
-            <div className="order-item">
-                <div className="order-item-img">
-                    <Image
-                        src={imageUrl}
-                        alt={name}
-                        className="object-contain"
-                        fill
-                    />
-                </div>
-                <div className="order-item-desc">
-                    <div className="item-category">{category}</div>
-                    <div className="item-name">{name}</div>
-                </div>
-                <div className="order-item-price">
-                    <div>{price}원</div>
-                </div>
-                <div className="order-item-quantity">
-                    <button className="quantity-button-m">-</button>
-                    <div className="item-quantity">{quantity}</div>
-                    <button className="quantity-button-p">+</button>
-                </div>
-            </div>
-        </>
-    )
+import Image from "next/image";
+import { CartItem } from "./cart-item";
+type OrderItemProps = {
+  item: CartItem;
+  onIncrease: () => void;
+  onDecrease: () => void;
+};
+export function OrderItem({ item, onIncrease, onDecrease }: OrderItemProps) {
+
+  return (
+    <div className="order-item">
+      <div className="order-item-img">
+        <Image
+            src={item.product.imageUrl || "/default-coffee-product.svg"}
+            alt={item.product.name || "기본 원두 상품 이미지"}
+            fill
+            sizes="80px"
+            className="object-cover"
+        />
+      </div>
+
+      <div className="order-item-desc">
+        <div className="item-category">{item.product.category}</div>
+        <div className="item-name">{item.product.name}</div>
+      </div>
+
+      <div className="item-price">{item.product.price.toLocaleString()}원</div>
+
+      <div className="order-item-quantity">
+        <button type="button" onClick={onDecrease} className="quantity-button-m" aria-label="수량 감소">
+          -
+        </button>
+        <div className="item-quantity">{item.quantity}</div>
+        <button type="button" onClick={onIncrease} className="quantity-button-p" aria-label="수량 증가">
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
