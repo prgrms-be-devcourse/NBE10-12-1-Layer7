912739eee36def240ce7def244254757157e75c0
whitelinkblaze <whitelinkblaze@gmail.com>
2026-06-08 09:14:16 +0900

    feat: 상품카드드 기본 이미지 반영 및 클릭시 장바구니로 가도록

--- frontend/customer/customer/src/app/products/product-card.tsx
+++ frontend/customer/customer/src/app/products/product-card.tsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
 interface ProductCardProps{
-    id: number;
-    name: string;
+    id?: number;
+    name?: string;
     price: number;
     category: string;
     imageUrl?: string;
+    onClick: () => void
 }
 
-export function ProductCard({name="none", price=0, category="미분류", imageUrl="/file.svg"}: ProductCardProps){
+export function ProductCard({name="none", price=0, category="미분류", imageUrl="/default-coffee-product.svg", onClick=(()=>{})}: ProductCardProps){
     return(
-        <div className="product-card">
+        <div className="product-card" onClick={onClick}>
             <div className="product-card-category">{category}</div>
             <div className="product-card-img">
-                <Image 
-                    src={imageUrl}
+                <Image
+                    src={imageUrl || "/default-coffee-product.svg"}
                     alt={name}
-                    className="object-contain"
                     fill
+                    className="object-cover"
                 />
-            </div>
+                </div>
             <div className="product-card-body">
                 <div className="product-name">{name}</div>
-                <div className="product-price">{price}</div>
+                <div className="product-price">{price.toLocaleString()} 원</div>
             </div>
         </div>
     );
