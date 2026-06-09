1fb3d9a01f550e10d831249e103a28f9940ddaf2
whitelinkblaze <whitelinkblaze@gmail.com>
2026-06-09 12:08:35 +0900

    fix: BasePage 영역 삭제

--- frontend/customer/customer/src/app/order/page.tsx
+++ frontend/customer/customer/src/app/order/page.tsx
@@ -44,94 +44,93 @@ export default function OrderPage({
     }, []);
 
     return (
-        <BasePage>
-            <section className="order-page">
-                <div className="order-block">
-                    <div className="order-area">
-                        <h2>장바구니</h2>
+        <section className="order-page">
+            <div className="order-block">
+                <div className="order-area">
+                    <h2>장바구니</h2>
 
-                        <div className="order-list">
-                            <ul>
-                                {items.length > 0 ? (
-                                    items.map((item) => (
-                                        <li key={item.product.id}>
-                                            <OrderItem
-                                                item={item}
-                                                onIncrease={() =>
-                                                    onIncrease(item.product.id)
-                                                }
-                                                onDecrease={() =>
-                                                    onDecrease(item.product.id)
-                                                }
-                                            />
-                                        </li>
-                                    ))
-                                ) : (
-                                    <p>장바구니가 비어 있습니다.</p>
-                                )}
-                            </ul>
-                        </div>
+                    <div className="order-list">
+                        <ul>
+                            {items.length > 0 ? (
+                                items.map((item) => (
+                                    <li key={item.product.id}>
+                                        <OrderItem
+                                            item={item}
+                                            onIncrease={() =>
+                                                onIncrease(item.product.id)
+                                            }
+                                            onDecrease={() =>
+                                                onDecrease(item.product.id)
+                                            }
+                                        />
+                                    </li>
+                                ))
+                            ) : (
+                                <p>장바구니가 비어 있습니다.</p>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+
+                <aside className="order-summary">
+                    <div className="summary-header">
+                        <h1>주문 요약</h1>
+                        <p>상품 요약 리스트</p>
                     </div>
 
-                    <aside className="order-summary">
-                        <div className="summary-header">
-                            <h1>주문 요약</h1>
-                            <p>상품 요약 리스트</p>
+                    <form className="order-form">
+                        <div>
+                            <label className="order-label">이메일</label>
+                            <input
+                                className="order-text-input"
+                                type="email"
+                                placeholder="이메일"
+                                value={member?.email ?? ""}
+                                readOnly
+                                autoFocus
+                            />
                         </div>
 
-                        <form className="order-form">
-                            <div>
-                                <label className="order-label">이메일</label>
-                                <input
-                                    className="order-text-input"
-                                    type="email"
-                                    placeholder="이메일"
-                                    value={member?.email ?? ""}
-                                    readOnly
-                                    autoFocus
-                                />
-                            </div>
+                        <div>
+                            <label className="order-label">주소</label>
+                            <input
+                                className="order-text-input"
+                                type="text"
+                                name="address"
+                                placeholder="주소"
+                                value={member?.address ?? ""}
+                                readOnly
+                            />
+                        </div>
 
-                            <div>
-                                <label className="order-label">주소</label>
-                                <input
-                                    className="order-text-input"
-                                    type="text"
-                                    name="address"
-                                    placeholder="주소"
-                                    value={member?.address ?? ""}
-                                    readOnly
-                                />
-                            </div>
+                        <div>
+                            <label className="order-label">우편번호</label>
+                            <input
+                                className="order-text-input"
+                                type="text"
+                                name="postal_code"
+                                placeholder="우편번호"
+                                value={member?.postalCode ?? ""}
+                                readOnly
+                            />
+                        </div>
 
-                            <div>
-                                <label className="order-label">우편번호</label>
-                                <input
-                                    className="order-text-input"
-                                    type="text"
-                                    name="postal_code"
-                                    placeholder="우편번호"
-                                    value={member?.postalCode ?? ""}
-                                    readOnly
-                                />
-                            </div>
+                        <p className="order-notice">
+                            매일 전날 오후 2시부터 당일 오후 2시까지의 주문을 모아서 처리합니다.
+                        </p>
 
-                            <p className="order-notice">
-                                매일 전날 오후 2시부터 당일 오후 2시까지의 주문을 모아서 처리합니다.
-                            </p>
+                        <div className="order-price">
+                            <span>총금액</span>
+                            <strong>{totalPrice.toLocaleString()}원</strong>
+                        </div>
 
-                            <div className="order-price">
-                                <span>총금액</span>
-                                <strong>{totalPrice.toLocaleString()}원</strong>
-                            </div>
-
-                            <button type="submit" className="order-button">
-                                결제하기
-                            </button>
-                        </form>
-                    </aside>
-                </div>
-            </section>
-        </BasePage>
+                        <button type="submit" className="order-button">
+                            결제하기
+                        </button>
+                    </form>
+                </aside>
+            </div>
+        </section>
+      
     );
 }
